494e21af3793607e2ed93cc6c20247225fe47b68
DESKTOP-K6LQKIG\kglas <kglasnapp@gmail.com>
2020-04-03 09:18:30 -0400

    Start to add form for editing ignores

--- graph.py
+++ graph.py
@@ -3,43 +3,41 @@ import numpy
 import matplotlib
 import matplotlib.pyplot as plt
 
-
-
 # get data from table
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
+# Create SQL string for reading data from data base
 s = "time,battery,current,"
 for i in range(0,16):
     s += "pdp%d," % (i)
 sql = "SELECT %s from allData_dslog;" % (s[0:-1])
 print("SQL:", sql)
+# Do the actual read from the data base
 cursor.execute(sql)
 alist = cursor.fetchall()
-print("Number of elements:", len(alist))
-print(alist[0])
-data1 = numpy.array(alist)
-print(data1[0])
-float_formatter = "{:.1f}".format
-numpy.set_printoptions(formatter={'float_kind':float_formatter})
+# Close out the data base
 cursor.close()
 conn.close()
-print(alist[0])
+print("Number of elements:", len(alist))
+data1 = numpy.array(alist) # Put the data in a numpy array in preparation for grpahing
+# Format the numpy array
+float_formatter = "{:.1f}".format
+numpy.set_printoptions(formatter={'float_kind':float_formatter})
+print(alist[0]) # Print the first line for debug purposes
+# Create the x axis for the Graph using the date from the data
 t = [row[0] for row in alist]
 for i in range(0,len(t)):
     t[i] = t[i][11:19]
 t = numpy.arange(0, len(t))
-print(t[0])
+# Create the Y data for the graph
 s = [row[2:10] for row in alist]
-for r in s:
-    if r[0] > 200:
-        print(r)
-
+# Graph the data
 fig, ax = plt.subplots()
 ax.plot(t, s)
-
 ax.set(xlabel='time (s)', ylabel='Current (Amps)',
        title='Current for the PDP')
 ax.grid()
-
+# Save a png of the graph for debug
 fig.savefig("test.png")
+# Place the graph on the screen
 plt.show()
--- ignores.py
+++ ignores.py
@@ -1,4 +1,61 @@
-import tkinter
+import tkinter as tk
+from tkinter import (Button, Checkbutton, Entry, Label, Menu, Text, Toplevel, Listbox,
+                     Scrollbar, ttk)
+
+
+def addNew():
+    pass
+    # rint(combo.get())
+    #mylbl = Label(root, text=combo.get())
+    # mylbl.pack()
+
+
+def isOk(r):
+    pass
+    #Aprint("isOK", r, combo.get())
+    return True
+
 
 def showForm():
-    print("Show the form")
+    global root, lstBox
+    root = tk.Tk()
+    newIgnoreV = tk.StringVar()
+    statusBarV = tk.StringVar()
+    root.title("Edit Ignores -- They must be regular expressions")
+    root.geometry("500x400")
+    
+    ignores = ['Keith', 'Sue', 'Doug', 'Chris']
+    for i in range(100):
+        ignores.append(i)
+    txtBox = ttk.Entry(root, width=40, textvariable=newIgnoreV)
+    addBtn = Button(root, text="Add", command=addNew)
+    saveBtn = Button(root, text="Save")
+    exitBtn = Button(root, text="Exit")
+    scrollbar = Scrollbar(root, orient=tk.VERTICAL)
+    lstBox = Listbox(root, selectmode=tk.EXTENDED,
+                     width=32, yscrollcommand=scrollbar.set)
+    scrollbar.config(command=lstBox.yview)
+    statusBar = Label(root, textvariable=statusBarV)
+   
+
+    txtBox.grid(row=0, column=0, padx=5, pady=5, sticky='new')
+    addBtn.grid(row=0, column=1, padx=5, pady=5,  sticky='ne')
+    saveBtn.grid(row=0, column=2,  padx=5, pady=5,  sticky='ne')
+    exitBtn.grid(row=0, column=3,  padx=5, pady=5,  sticky='ne')
+    lstBox.grid(row=1, column=0, columnspan=3, sticky='nesw', padx=5, pady=5)
+    scrollbar.grid(row=1, column=3, sticky='nesw', padx=15, pady=5)
+    statusBar.grid(row=2, column=0, columnspan=2, sticky='ws', padx=5, pady=5)
+    
+    
+    root.grid_rowconfigure(1, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    
+    statusBarV.set("Status: %d ignores" % (len(ignores)))
+    
+    for x in ignores:
+        lstBox.insert(tk.END, x)
+
+    root.mainloop()
+
+
+showForm()
